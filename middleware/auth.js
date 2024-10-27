b2b7066e635439a1deb0a6e7a458e1587feea706
RoxanaDC <cretu2roxana@gmail.com>
2024-10-27 04:20:04 +0100

    security .env + dotenv install

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,15 @@
 const jsonWebToken = require("jsonwebtoken");
-console.log("execut autentificare");
+require("dotenv").config();
+
+const { SECRET_TOKEN } = process.env;
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jsonWebToken.verify(token, "RANDOM_SECRET_TOKEN");
+    const decodedToken = jsonWebToken.verify(token, SECRET_TOKEN);
     const userId = decodedToken.userId;
     req.auth = { userId: userId };
     next();
   } catch (error) {
-    console.log("eroare eutentificare");
     res.status(401).json({ error: "requête non authorisé" });
   }
 };
--- middleware/multer_and_sharp_config.js
+++ middleware/multer_and_sharp_config.js
@@ -1,6 +1,7 @@
 const multer = require("multer");
 const sharp = require("sharp");
 const path = require("path");
+const fs = require("fs");
 
 const MIME_TYPES = {
   "image/jpg": "jpg",
@@ -11,54 +12,48 @@ const MIME_TYPES = {
 // configuration pour multer: save l'image brute dans le dossier "images"
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "images"); // le dossier destination pour les images bruts
+    callback(null, "images");
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_").split(".")[0];
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + "_" + Date.now() + "." + extension);
+    callback(null, `${name}_${Date.now()}.${extension}`);
   },
 });
 
-console.log("execut multer");
-
-// le middleware multer pour upload
 const upload = multer({ storage }).single("image");
 
-// Middleware pour processer l'image en utilisant sharp
-const processImage = (req, res, next) => {
-  if (!req.file) {
-    return next(); // Si l'image n'existe pas, on passe au middleware suivant
-  }
-
-  // on construit un nom pour le fichier processé
-  const transformedFilename = `${req.file.filename.split(".")[0]}.webp`;
-
-  // on specifie la route pour le fichier processé
-  const outputPath = path.join(__dirname, "..", "images", transformedFilename);
-
-  // On processe l'image: resize, conversion webp et save
-  sharp(req.file.path)
-    .resize({ height: 500 })
-    .webp({ quality: 70 })
-    .toFile(outputPath, (error) => {
-      if (error) {
-        return res.status(500).json({ error });
-      }
-
-      // On actualise req.file pour reflecter le fichier processé
-      req.file.filename = transformedFilename;
-      req.file.path = outputPath;
-      next();
-    });
-};
-
-// export les middlewares pour etre utilisées ensemble
+// le middleware multer pour upload et modification de l'image
 module.exports = (req, res, next) => {
   upload(req, res, (err) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    processImage(req, res, next); // on aplique la processation sharp après upload
+    if (err) return res.status(500).json({ error: err.message });
+    if (!req.file) return next();
+
+    // la construction du nom de l'image .webp
+    const webpFilename = `${req.file.filename.split(".")[0]}.webp`;
+    const outputPath = path.join(__dirname, "..", "images", webpFilename);
+
+    // modifier l'image avec sharp et save le resultat .webp
+    sharp(req.file.path)
+      .resize({ height: 500 })
+      .webp({ quality: 70 })
+      .toFile(outputPath, (error) => {
+        if (error) return res.status(500).json({ error });
+
+        // Supprimer l'image originale .jpg/.png après le save de l'image .webp
+        console.log(req.file.path);
+        fs.unlink(req.file.path, (unlinkError) => {
+          if (unlinkError)
+            console.error(
+              "Erreur à la suppression de l'image originale",
+              unlinkError
+            );
+        });
+
+        // L'actualisation du `req.file` avec les infos de l'image .webp
+        req.file.filename = webpFilename;
+        req.file.path = outputPath;
+        next();
+      });
   });
 };
